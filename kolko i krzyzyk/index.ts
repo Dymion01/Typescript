26fb6fcb61c6b1b6d19b08eed9f33d2a6788eced
Dymion <damianskorupa4@gmail.com>
2020-03-29 17:24:05 +0200

    making board and cells

---
+++ kolko i krzyzyk/index.ts
@@ -0,0 +1,20 @@
+class Cell {
+  
+}
+
+class Board {
+     tab = [Cell ,Cell, Cell, Cell ,Cell , Cell, Cell ,Cell, Cell ]
+     constructor 
+}
+
+window.onload = () => {
+    let cell11: Cell  = <HTMLElement>document.getElementById("cell11");
+    let cell12: Cell  = <HTMLElement>document.getElementById("cell12");
+    let cell13: Cell  = <HTMLElement>document.getElementById("cell13");
+    let cell21: Cell  = <HTMLElement>document.getElementById("cell21");
+    let cell22: Cell  = <HTMLElement>document.getElementById("cell22");
+    let cell23: Cell  = <HTMLElement>document.getElementById("cell23");
+    let cell31: Cell  = <HTMLElement>document.getElementById("cell31");
+    let cell32: Cell  = <HTMLElement>document.getElementById("cell32");
+    let cell33: Cell  = <HTMLElement>document.getElementById("cell33");
+}
